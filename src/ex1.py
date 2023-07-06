167480688dc6cc27def32325b405ca987834af14
Rosie Gonzalez <rgonzalez@xpanxion.com>
2023-07-05 16:30:35 -0800

    ex1

--- src/ex1.py
+++ src/ex1.py
@@ -1,14 +1,25 @@
 from ValidationException import ValidationException
 
 
-def validate_file():
-    pass # TODO
+def validate_file(file_name):
+    lines = []
 
+    with open(file_name, "r") as file1:
+        while True:
+            line = file1.readline().split()
+
+            if not line:
+                break
+            lines.append(line)
+
+    for i in range(len(lines)):
+        if "." in lines[i][1]:
+            raise ValidationException(f"Invalid Milage: {lines[i][1]}")
 
 
 def ex1():
     try:
-        validate_file("input.txt")
+        validate_file("../files/input.txt")
     except ValidationException as ve:
         print(ve)
 
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ValeriePerezPimentel\Code\python-3-exercises\venv
+set VIRTUAL_ENV=C:\Users\RosieGonzalez\Code\python\python-3-exer\python-3-exercises\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
