57e72e74a09ab3c1cc7be4f8e453bea472bf55ed
Dhruv T <36724675+L05Dhruv@users.noreply.github.com>
2023-07-07 16:09:00 -0400

    solutions to ex1/ex2

--- src/ex1.py
+++ src/ex1.py
@@ -1,14 +1,25 @@
 from ValidationException import ValidationException
 
+def validate_file(file_name):
+    file1 = open(file_name)
 
-def validate_file():
-    pass # TODO
+    for count, line in enumerate(file1):
+        # print(count, line, end="\n")
+        if count != 0:
+            line_values = line.split(',')
+            try:
+                int(line_values[1])
+            except:
+                file1.close()
+                raise ValidationException(f"INVALID MILEAGE: ${line_values[1]}")
 
+    file1.close()
+    return True
 
 
 def ex1():
     try:
-        validate_file("input.txt")
+        validate_file("./files/input.txt")
     except ValidationException as ve:
         print(ve)
 
--- src/ex2.py
+++ src/ex2.py
@@ -1,10 +1,34 @@
+import numpy as np
+import csv
 
 def find_total_visits():
-    pass # TODO
+    visit_counter = 0
+    # parse each file (3)
+    for week_number in range(1, 4):
+        file_path = f"./files/week-{week_number}.csv"
+        
+        # opened each file and extracted data for each row as an object
+        with open(file_path, 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                # sum every " 1" and update counter
+                days_of_week = [' S', ' M', ' T', ' W', ' Th', ' F', ' S']
+                visits_list = [int(row[day]) for day in days_of_week]
+                visit_counter += sum(visits_list)
 
+    # return sum
+    return visit_counter
 
 def ex2():
     total = find_total_visits()
     print(f"Total visits: {total}.")
-
 ex2()
+
+
+people_list = [
+    {'name': 'alice', 'age': 20},
+    {'name': 'dave', 'age': 29}
+]
+
+new = [person['age'] for person in people_list]
+# print(new)
